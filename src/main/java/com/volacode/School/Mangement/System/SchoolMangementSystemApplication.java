1199208aefd929171c3e513b36781ed4d0f186de
Victor Olamide <akinnusivictor098@gmail.com>
2023-02-06 13:11:20 +0100

    first commit

---
+++ src/main/java/com/volacode/School/Mangement/System/SchoolMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.volacode.School.Mangement.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMangementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volacode/School/Mangement/System/SchoolMangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volacode.School.Mangement.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMangementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
